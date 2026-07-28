2afc87487f784394d764772ddd21e66ff787abe4
Josias <josiasguilherme08@gmail.com>
2026-07-28 14:51:41 -0300

    Feature: Header - Adcionado conteudo de menu de navegação e o botão que levará á rota de Doações.

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -14,7 +14,39 @@ export default function Header(){
 
             <nav className="menu">
 
+                <div className="menu-item">
+                    <span> Animais </span>
+
+                    <div className="submenu"></div>
+
+                    <Link href="/animais/cadastrar"> Cadastrar novo Animal </Link>
+                    <Link href="/animais/necessidades"> Cadastrar necessidades do Animal </Link>
+                </div>
+
+                <div className="menu-item">
+                    <span> Cuidadores </span>
+
+                    <div className="submenu"></div>
+
+                    <Link href="/cuidadores/cadastrar"> Cadastrar novo cuidador </Link>
+                    <Link href="/cuidadores/vincular"> Vincular cuidador </Link>
+                </div>
+
+                <div className="menu-item">
+                    <span> Suprimentos </span>
+
+                    <div className="submenu"></div>
+
+                    <Link href="/suprimentos/cadastrar"> Cadastrar novo suprimento </Link>
+                    <Link href="/suprimentos/checar"> Checar suprimento </Link>
+                </div>
+
+                
+
             </nav>
+
+            <div className="donations"></div>
+                <Link href="/doacoes"> Doações </Link>
 
         </div>
 
