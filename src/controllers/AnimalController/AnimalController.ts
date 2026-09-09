7d4e2b972e124fca150bf27c0f58d3f3af35c215
Josias <a96294429@rn.senac.local>
2026-09-09 16:45:51 -0300

    Foi iniciada a criação da classe AnimalController

---
+++ src/controllers/AnimalController/AnimalController.ts
@@ -0,0 +1,96 @@
+import { AnimalService } from "@/services/AnimalService/AnimalService";
+import { statSync } from "fs";
+import { NextRequest, NextResponse } from "next/server";
+
+export class AnimalController {
+
+    private animalService: AnimalService;
+
+    constructor(){
+        this.animalService = new AnimalService();
+    }
+
+
+    public async listarTodos(req: NextRequest){
+
+        try{
+
+            const animais = await this.animalService.listarTodos();
+
+            return NextResponse.json(animais, {status: 200});
+
+        } catch(error: unknown) {
+            const mensagem = error instanceof Error ? error.message : "Error interno ao buscar a lista de animais no servidor.";
+
+                return NextResponse.json({ error: mensagem}, { status: 500 });
+
+        }
+
+    }
+
+
+    public async cadastrar(req: NextRequest) {
+
+        try{
+
+            const body = await req.json();
+
+            const {nome, tipo, raca, sexo, data_nascimento, data_entrada_abrigo, observacoes, id_cuidador} = body;
+
+            const animalSalvo = await this.animalService.cadastrarAnimal({
+                nome, tipo, raca, sexo, data_nascimento: new Date(data_nascimento), data_entrada_abrigo: new Date(), observacoes}, Number(id_cuidador));
+
+        } catch (error: unknown) {
+
+            const mensagem = error instanceof Error ? error.message : "Não foi possivel concluir o cadastro do animal devido a um erro inesperado";
+
+            return NextResponse.json({ error: mensagem }, { status: 400})
+        }
+    }
+
+    public async buscarCompleto(req: NextRequest, params: { id_animal: string}) {
+
+        try {
+
+            const id_animal = Number(params.id_animal);
+
+            const animal = this.animalService.buscarAnimalCompleto(id_animal);
+
+            if(!animal){
+                return NextResponse.json({ error: "Nenhum animal foi encontrado com o ID especifico."}, { status: 400 })
+            } 
+
+            return NextResponse.json(animal, { status: 200 });
+
+
+        } catch(error: unknown) {
+
+            const mensagem = error instanceof Error ? error.message : "ID do animal fornecido é inválido";
+
+            return NextResponse.json(this.animalService, { status: 500});
+
+        }
+    }
+
+    public async listarPorCuidador(req: NextRequest, params: { id_cuidador: string}) {
+
+        try {
+            const id_cuidador = Number(params.id_cuidador);
+
+            if(isNaN(id_cuidador)){
+                return NextResponse.json({error: "O ID do cuidador fornecido é inválido!" }, { status: 400});
+            }
+
+            const animais = await this.animalService.listarPorCuidador(id_cuidador);
+
+            return NextResponse.json(animais, { status: 200});
+
+
+        } catch (error: unknown) {
+
+            const mensagem = error instanceof Error ? error.message : "Erro interno ao listar os animais vinculados a este cuidador";
+
+            return NextResponse.json({ error: mensagem }, { status: 500 })
+        }
+    } 
+}
